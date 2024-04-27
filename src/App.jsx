2a02b8b995e994a94a57b3b44b18e1f09a20df9e
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-04-27 22:14:00 +0500

    ini update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {  Route, Routes } from "react-router-dom"
 import Home_page from './components/home_page'
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-    <BrowserRouter>
     <Navbar/>
       <Routes>
         <Route index element={<Home_page/>}/>
       </Routes>
-    </BrowserRouter>
     </>
   )
 }
