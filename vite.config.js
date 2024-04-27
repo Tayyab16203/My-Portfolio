816d92cb37de7e661f28668934b4471c2d239497
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-04-27 22:44:44 +0500

    ini

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import { VitePluginGhPages } from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA(),
+    VitePluginGhPages({
+      // Additional configuration options here if needed
+    }),
+  ],
+});
