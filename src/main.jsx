52c32b169be29bdbf6a019acbcba006465aacba5
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-04-27 22:00:17 +0500

    all back

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,7 @@ import './index.css'
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
-import { VitePluginGhPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePWA(),
-    VitePluginGhPages({
-      // Additional configuration options here if needed
-    }),],
+  plugins: [react()],
 })
