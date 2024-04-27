5ee114351f3251148a57910f5f5882ab5e091df2
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-04-27 21:55:44 +0500

    packge.json changes and some more

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+import { VitePluginGhPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA(),
+    VitePluginGhPages({
+      // Additional configuration options here if needed
+    }),],
 })
