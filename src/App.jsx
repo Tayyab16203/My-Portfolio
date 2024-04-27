17acb1bd2681ea37d14ae7559326293d31fc0562
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-04-27 21:31:08 +0500

    hashbrowser in myportfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import Home_page from './components/home_page'
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <HashRouter>
     <Navbar/>
       <Routes>
         <Route index element={<Home_page/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </>
   )
 }
