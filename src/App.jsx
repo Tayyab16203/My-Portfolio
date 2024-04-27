5da62133204dd957c5af9eecb6a6d0d6a02ab53f
Tayyab16203 <rehmantayyab0340@gmail.com>
2024-04-27 13:07:30 +0500

    wapas update browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { HashRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home_page from './components/home_page'
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-    <HashRouter>
+    <BrowserRouter>
     <Navbar/>
       <Routes>
         <Route index element={<Home_page/>}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </>
   )
 }
